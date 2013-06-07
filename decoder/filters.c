52f7663dae01c3fd711ec68bca40135732f55c79
Raphael Canut <nhwcodec@gmail.com>
2013-06-07 14:28:49 +0200

    fix rounding in wavelet 9/7 filter

--- decoder/filters.c
+++ decoder/filters.c
@@ -46,6 +46,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "codec.h"
 #include "wavelets.h"
--- encoder/filters.c
+++ encoder/filters.c
@@ -47,6 +47,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "codec.h"
 #include "wavelets.h"
