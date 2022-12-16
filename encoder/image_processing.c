6449cf70d49310b1632ee9befed8b2f9f2fd7463
Raphael Canut <nhwcodec@gmail.com>
2022-12-16 20:36:52 +0100

    version 0.2.8.2 - better pre_processing

--- encoder/image_processing.c
+++ encoder/image_processing.c
@@ -2,8 +2,8 @@
 ****************************************************************************
 *  NHW Image Codec 													       *
 *  file: image_processing.c  										       *
-*  version: 0.2.8.1 						     		     			   *
-*  last update: $ 12102022 nhw exp $							           *
+*  version: 0.2.8.2 						     		     			   *
+*  last update: $ 12162022 nhw exp $							           *
 *																		   *
 ****************************************************************************
 ****************************************************************************
@@ -871,6 +871,10 @@ void pre_processing(image_buffer *im)
 						if (res>0 && count>0) im->im_jpeg[scan]--;
 						else if (res<0 && count<0) im->im_jpeg[scan]++;
 					}
+					else if (j<((2*IM_DIM)-6) && abs(nhw_kernel[scan+1])>(sharpness+160) && abs(nhw_kernel[scan+2])>(sharpn2+20)) 
+					{
+						j--;scan--;
+					}
 				}
 				else if (abs(count)>(sharpness+160) && abs(res)>sharpn2 && abs(res)<=(sharpn2+20))
 				{
@@ -879,7 +883,7 @@ void pre_processing(image_buffer *im)
 						if (res>0 && count>0) im->im_jpeg[scan-1]--;
 						else if (res<0 && count<0) im->im_jpeg[scan-1]++;
 					}
-					else 
+					else
 					{
 						j--;scan--;
 					}
