4b37844c1eb4504f64e0a8eb177ba3f890610889
Raphael Canut <nhwcodec@gmail.com>
2025-01-15 20:35:30 +0100

    version 0.3.0-rc48
    Continue to fine-tune the nhw_kernel weights

--- encoder/image_processing.c
+++ encoder/image_processing.c
@@ -2,8 +2,8 @@
 ****************************************************************************
 *  NHW Image Codec 													       *
 *  file: image_processing.c  										       *
-*  version: 0.3.0-rc47 						     		     			   *
-*  last update: $ 01102025 nhw exp $							           *
+*  version: 0.3.0-rc48 						     		     			   *
+*  last update: $ 01152025 nhw exp $							           *
 *																		   *
 ****************************************************************************
 ****************************************************************************
@@ -557,7 +557,7 @@ void im_recons_wavelet_band(image_buffer *im)
 
 void pre_processing(image_buffer *im)
 {
-    int i,j,scan,res,res2,res3,res4,count,e=0,f=0,a=0,sharpness=0,sharpn2=0,n1,t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23;
+    int i,j,scan,res,res2,res3,res4,count,e=0,f=0,a=0,sharpness=0,sharpn2=0,n1,t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24;
     int nps,w1,w2,w3,w4,w5,w6,w7,w8;
 	short *nhw_process, *nhw_kernel;
 	char lower_quality_setting_on, *nhw_sharp_on;
@@ -767,7 +767,7 @@ void pre_processing(image_buffer *im)
 	
 	if (im->setup->quality_setting<=LOW4) nhw_sharp_on=(char*)calloc(4*IM_SIZE,sizeof(char));
 						
-	for (i=(2*IM_DIM),t1=0,t2=0,t3=0,t4=0,t5=0,t6=8,t7=0,t8=0,t9=0,t10=10,t11=15,t12=0,t13=0,t14=0,t15=0,t16=0,t17=0,t18=8,t19=0,t20=0,t21=0,t22=0,t23=0;i<((4*IM_SIZE)-(2*IM_DIM));i+=(2*IM_DIM))
+	for (i=(2*IM_DIM),t1=0,t2=0,t3=0,t4=0,t5=0,t6=8,t7=0,t8=0,t9=0,t10=10,t11=15,t12=0,t13=0,t14=0,t15=0,t16=0,t17=0,t18=8,t19=0,t20=0,t21=0,t22=0,t23=0,t24=0;i<((4*IM_SIZE)-(2*IM_DIM));i+=(2*IM_DIM))
 	{
 		for (scan=i+1,j=1;j<((2*IM_DIM)-2);j++,scan++)
 		{   
@@ -1178,10 +1178,54 @@ void pre_processing(image_buffer *im)
 							
 									t11 = 12;
 									
-									if (!(t7&1)) t16 = 1;
-									else t16 = 0;
-									
-									t7++;
+									if (!t24)
+									{
+										 t16 = 1;
+										 
+										 t24 = 1;
+									}
+									else if (t24==1)
+									{
+										 t16 = 1;
+										 
+										 t24 = 2;
+									}
+									else if (t24==2)
+									{
+										 t16 = 1;
+										 
+										 t24 = 3;
+									}
+									else if (t24==3)
+									{
+										 t16 = 0;
+										 
+										 t24 = 4;
+									}
+									else if (t24==4)
+									{
+										 t16 = 1;
+										 
+										 t24 = 5;
+									}
+									else if (t24==5)
+									{
+										 t16 = 0;
+										 
+										 t24 = 6;
+									}
+									else if (t24==6)
+									{
+										 t16 = 1;
+										 
+										 t24 = 7;
+									}
+									else if (t24==7)
+									{
+										 t16 = 0;
+										 
+										 t24 = 4;
+									}
 								}
 							}
 							else
