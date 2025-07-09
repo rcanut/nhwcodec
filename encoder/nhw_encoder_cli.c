764764dd3c48e7541560e89dd84bbf9c401a894f
Raphael Canut <nhwcodec@gmail.com>
2025-07-09 18:11:16 +0200

    version 0.3.0-rc72
    Psy-tuning of the nhw_kernel weights

--- encoder/nhw_encoder_cli.c
+++ encoder/nhw_encoder_cli.c
@@ -2,8 +2,8 @@
 ****************************************************************************
 *  NHW Image Codec                                                         *
 *  file: nhw_encoder_cli.c                                                 *
-*  version: 0.3.0-rc71                                                     *
-*  last update: $ 07072025 nhw exp $                                       *
+*  version: 0.3.0-rc72                                                     *
+*  last update: $ 07092025 nhw exp $                                       *
 *                                                                          *
 ****************************************************************************
 ****************************************************************************
@@ -51,7 +51,7 @@
 #include "codec.h"
 
 #define PROGRAM "nhw-enc"
-#define VERSION "0.3.0-rc71"
+#define VERSION "0.3.0-rc72"
 
 #define NHW_QUALITY_MIN LOW20
 #define NHW_QUALITY_MAX HIGH3
