4125ee3d975369af4ad697e813cafa08bee5077f
Raphael Canut <nhwcodec@gmail.com>
2026-05-13 19:34:42 +0200

    version 0.3.2+18
    Continue to fine-tune the nhw_kernel weights

--- encoder/nhw_encoder_cli.c
+++ encoder/nhw_encoder_cli.c
@@ -2,8 +2,8 @@
 ****************************************************************************
 *  NHW Image Codec                                                         *
 *  file: nhw_encoder_cli.c                                                 *
-*  version: 0.3.2+17                                                       *
-*  last update: $ 05112026 nhw exp $                                       *
+*  version: 0.3.2+18                                                       *
+*  last update: $ 05132026 nhw exp $                                       *
 *                                                                          *
 ****************************************************************************
 ****************************************************************************
@@ -51,7 +51,7 @@
 #include "codec.h"
 
 #define PROGRAM "nhw-enc"
-#define VERSION "0.3.2+17"
+#define VERSION "0.3.2+18"
 
 #define NHW_QUALITY_MIN LOW20
 #define NHW_QUALITY_MAX HIGH3
